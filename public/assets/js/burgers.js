cee1db3206037e7379139ae5301b9d0a6942dda4
Joe Christopher <joechristopher@icloud.com>
2018-04-21 23:19:12 -0400

    updated css

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -27,7 +27,7 @@ $(function() {
     event.preventDefault();
 
     var newBurger = { burger_name: $("#addBurger").val().trim()};
-   
+    console.log("newBurger = " + newBurger.burger_name);
     // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
