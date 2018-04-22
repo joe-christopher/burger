619bd003af552508b8050cde07ad9fd8f22f77a6
Joe Christopher <joechristopher@icloud.com>
2018-04-22 14:23:19 -0400

    added JAWSDB to connection.js

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# burger
+Eat-Da-Burger! application
+
+Eat-Da-Burger! is a restaurant app that lets users input the names of burgers they'd like to eat.
+
+Whenever a user submits a burger's name, your app will display the burger on the left side of the page -- waiting to be devoured.
+
+Each burger in the waiting area also has a `Devour it!` button. When the user clicks it, the burger will move to the right side of the page.
+
+Technologies used: MySQL, Node, Express, Handlebars, ORM, JQuery, CSS, HTML
+
+
--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
 var mysql = require("mysql");
+var connection;
 
+if (process.env.JAWSDB_URL)
+{
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else
+{
+    connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+    // Your username
+    user: "root",
 
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "root",
-  database: "burgers_db",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
-});
-
+    // Your password
+    password: "root",
+    database: "burgers_db",
+    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+  });
+}
 // Make connection.
 connection.connect(function(err) {
     if (err) {
